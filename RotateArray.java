f55ce1a2aa6951512fa16dd6c2594a5f03d997b8
Chirag Das <96284094+Raptor-52@users.noreply.github.com>
2023-05-16 02:12:04 +0530

    Add files via upload

---
+++ RotateArray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int rotate[]=new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            rotate[(i + k) % nums.length] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = rotate[i];
+        }
+    }
+}
