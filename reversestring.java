126ef70e69d14a010c93492ceb256247c2136023
Chirag Das <96284094+Raptor-52@users.noreply.github.com>
2023-05-19 10:56:05 +0530

    Add files via upload

---
+++ reversestring.java
@@ -0,0 +1,15 @@
+class reversestring{
+    public void reverseString(char[] s) {
+      int low=0;
+      int high=s.length-1;
+      while(low<=high){
+          char temp=s[low];
+          s[low]=s[high];
+          s[high]=temp;
+
+          low+=1;
+          high-=1;
+      }
+    }
+
+}
